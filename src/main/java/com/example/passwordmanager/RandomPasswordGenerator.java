0e28f7a60178a56b14914433afbd7ddcdcc7a1db
Keven Qiu <44330818+qkeven1234@users.noreply.github.com>
2021-10-16 01:25:59 -0600

    Feature/random password generation (#1)
    * Implemented random password generator with lower and upper case, numeric, and special character functions.
    
    * Added unit tests for random password generator
    
    * Fixed formatting with save actions and removed comments.

--- src/main/java/com/example/passwordmanager/RandomPasswordGenerator.java
+++ src/main/java/com/example/passwordmanager/RandomPasswordGenerator.java
@@ -1,13 +1,58 @@
 package com.example.passwordmanager;
 
+import java.util.Random;
+
 public class RandomPasswordGenerator {
     private String password;
     private int length;
-    public static String characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTYVWXYZ0123456789";
+    private String characters;
+    private boolean lowerAlpha;
+    private boolean upperAlpha;
+    private boolean numeric;
+    private boolean special;
+    private final Random random;
+
+    public static String lowerCaseCharacters = "abcdefghijklmnopqrstuvwxyz";
+    public static String upperCaseCharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    public static String numbers = "0123456789";
     public static String specialCharacters = "(,._-*~\\\"<>/|!@#$%^&)+='";
 
-    public RandomPasswordGenerator(int length) {
-        password = "";
+    public RandomPasswordGenerator() {
+        assignMemberVariables(0, false, false, false, false);
+        this.random = new Random();
+    }
+
+    private String createAvailableCharacters() {
+        String charactersAllowed = "";
+        if (lowerAlpha) charactersAllowed += lowerCaseCharacters;
+        if (upperAlpha) charactersAllowed += upperCaseCharacters;
+        if (numeric) charactersAllowed += numbers;
+        if (special) charactersAllowed += specialCharacters;
+        return charactersAllowed;
+    }
+
+    private void createRandomPassword() {
+        StringBuilder stringBuilder = new StringBuilder(password);
+        for (int i = 0; i < length; ++i)
+            stringBuilder.append(characters.charAt(random.nextInt(characters.length())));
+        password = stringBuilder.toString();
+    }
+
+    public String getPassword(int length, boolean lowerAlpha,
+        boolean upperAlpha, boolean numeric, boolean special) {
+        assignMemberVariables(length, lowerAlpha, upperAlpha, numeric, special);
+        createRandomPassword();
+        return password;
+    }
+
+    private void assignMemberVariables(int length, boolean lowerAlpha,
+        boolean upperAlpha, boolean numeric, boolean special) {
+        this.password = "";
         this.length = length;
+        this.lowerAlpha = lowerAlpha;
+        this.upperAlpha = upperAlpha;
+        this.numeric = numeric;
+        this.special = special;
+        this.characters = createAvailableCharacters();
     }
 }
